6d868ed8493e1bc6432ae39ead1e062fafedec6e
marco.padeiro <2211868@my.ipleiria.pt>
2023-07-06 00:00:49 +0100

    Initial commit with the base of the project

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # batt_stats
-A tool made in Rust to log battery drain info.
+A Linux notebook battery drain logger made in Rust. 
+
+## [WIP] Installation
+Clone the repository
+```
+git clone https://github.com/marcofpadeiro/batt_stats
+```
+And build the project using cargo
+```
+cd batt_stats
+cargo build --release
+```
+After the build the executable will be at `./target/release/batt_stats`
+
+## Usage
+The script just needs to be running and it will automatically detect if the notebook is charging or discharging and log into `~/.cache/batt_stats`
+
+To automatically run this script on startup you can create a simple systemd service
+```
+/etc/systemd/system/batt_stats.service
+```
+```
+[Unit]
+Description=A battery percentage logger
+
+[Service]
+ExecStart=/path/to/script <number_of_seconds_to_wait_between_log>
+Restart=always
+
+[Install]
+WantedBy=default.target
+```
+And enable it 
+```
+systemctl enable batt_stats.service
+```
---
+++ src/main.rs
@@ -0,0 +1,87 @@
+use battery::units::power::watt;
+use battery::units::ratio::percent;
+use battery::Manager;
+use battery::State;
+use chrono::Local;
+use std::env;
+use std::fs::{self, OpenOptions};
+use std::io::{self, Write};
+use std::path::Path;
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() -> battery::Result<()> {
+    let wait_time: u64 = match env::args().nth(1) {
+        Some(arg1) => arg1
+            .parse()
+            .expect("Not a valid number of minutes passed as an argument"),
+        None => 5,
+    };
+
+    if !Path::new("logs").exists() {
+        fs::create_dir_all("logs").expect("Failed to create cache directory");
+    }
+
+    let (mut battery, manager) = get_battery().expect("Failed to get battery!");
+
+    let mut log_file: Box<dyn Write> = Box::new(io::stdout());
+    let mut reset_file = true;
+
+    // Infinite loop to detect the current state of the battery
+    loop {
+        // If the battery is discharging and it just came from a different state, it creates a new log file
+        if battery.state() == State::Discharging {
+            if reset_file {
+                log_file = Box::new(manage_log_file());
+                reset_file = false;
+            }
+            let current_percent = battery.state_of_charge().get::<percent>();
+            let current_rate = battery.energy_rate().get::<watt>();
+
+            writeln!(
+                log_file,
+                "{} - Battery: {:?}% {:?}w",
+                Local::now().format("%Y-%m-%d %H:%M:%S"),
+                current_percent,
+                current_rate,
+            )
+            .expect("Failed to write to the log file.");
+        } else {
+            // If the state is Charging, create a new file next time the state is Discharge
+            reset_file = true;
+        }
+
+        sleep(Duration::from_secs(wait_time));
+        manager.refresh(&mut battery)?;
+    }
+}
+
+fn get_battery() -> battery::Result<(battery::Battery, battery::Manager)> {
+    let manager = Manager::new()?;
+
+    match manager.batteries()?.next() {
+        Some(Ok(battery)) => Ok((battery, manager)),
+        Some(Err(e)) => {
+            eprintln!("Unable to access battery information");
+            Err(e.into())
+        }
+        None => {
+            eprintln!("Unable to find any batteries");
+            Err(io::Error::new(io::ErrorKind::NotFound, "No batteries found").into())
+        }
+    }
+}
+
+fn manage_log_file() -> std::fs::File {
+    let current_time = Local::now().format("%Y-%m-%d %H:%M:%S");
+    let log_file_path: String = format!("logs/{}.txt", current_time);
+    let log_file_path = Path::new(&log_file_path);
+
+    // Create or open the log file
+    OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(log_file_path)
+        .expect("Failed to open the log file.")
+}
+
